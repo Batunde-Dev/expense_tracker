3530697e290580d9eb0bfa0d50f2c10e8809b278
Batunde-Dev <batunde.dev@gmail.com>
2023-09-12 22:03:33 +0100

    Modify the firebase config file

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -15,12 +15,12 @@ const firebaseConfig = {
   storageBucket: "expense-tracker-3b26e.appspot.com",
   messagingSenderId: "944857201019",
   appId: "1:944857201019:web:91eac41747f03fb10a25de",
-  measurementId: "G-2PELEMJP2L"
+ 
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
